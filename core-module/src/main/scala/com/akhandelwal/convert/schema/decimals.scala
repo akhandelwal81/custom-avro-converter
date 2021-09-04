80e1e1e155dacef3678eff572dca662a6f8e3697
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-04 14:07:59 +0100

    Define a decimal case class for schema conversion

---
+++ core-module/src/main/scala/com/akhandelwal/convert/schema/decimals.scala
@@ -0,0 +1,18 @@
+package com.akhandelwal.convert
+import org.apache.avro.{LogicalTypes, Schema, SchemaBuilder}
+
+case class ScalePrecision(scale: Int, precision: Int)
+
+object ScalePrecision {
+  given default: ScalePrecision = ScalePrecision(2, 8)
+}
+
+trait DecimalSchemas:
+  given(using scapre: ScalePrecision): SchemaFor[Decimal] = new DecimalSchemaFor(scapre)
+
+class DecimalSchemaFor(scapre: ScalePrecision) extends SchemaFor[Decimal] :
+  override def schema: Schema = LogicalTypes.decimal(scapre.precision, sp.scale).addToSchema(SchemaBuilder.builder.bytesType)
+
+object Decimals {
+  val AsString: SchemaFor[Decimal] = SchemaFor[Decimal](SchemaBuilder.builder.stringType)
+}
